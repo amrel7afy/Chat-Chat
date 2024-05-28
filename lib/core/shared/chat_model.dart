49c4d2fba46dd486600d6f3c3b00abeb1eaa2181
amrel7afy <ame.el7afy@gmail.com>
2024-05-28 17:43:48 +0300

    - add chatting_cubit.dart.

---
+++ lib/core/shared/chat_model.dart
@@ -0,0 +1,56 @@
+
+import 'package:new_chat_with_me/core/shared/user_model.dart';
+
+class ChatModel {
+  final String phoneNumber;
+  final String name;
+  final String profilePic;
+  int unreadMessagesCount;
+
+  ChatModel(
+      {required this.name,
+      required this.phoneNumber,
+      required this.profilePic,
+      required this.unreadMessagesCount});
+
+  ChatModel.fromUserModel(UserModel userModel)
+      : phoneNumber = userModel.phoneNumber,
+        name = userModel.name,
+        profilePic = userModel.profilePic,
+        unreadMessagesCount = 0;
+
+  factory ChatModel.fromJson(Map<String, dynamic> json) {
+    final phoneNumber = json['phoneNumber'];
+    final name = json['name'];
+    final profilePic = json['profilePic'];
+    final unreadMessagesCount = json['unreadMessagesCount'];
+
+    if (phoneNumber is! String) {
+      throw FormatException(
+          'Invalid JSON: required "phoneNumber" field of type String in $json');
+    }
+    if (name is! String) {
+      throw FormatException(
+          'Invalid JSON: required "name" field of type String in $json');
+    }
+    if (profilePic is! String) {
+      throw FormatException(
+          'Invalid JSON: required "profilePic" field of type String in $json');
+    }
+
+    return ChatModel(
+        phoneNumber: phoneNumber,
+        name: name,
+        profilePic: profilePic,
+        unreadMessagesCount: unreadMessagesCount);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'phoneNumber': phoneNumber,
+      'name': name,
+      'profilePic': profilePic,
+      'unreadMessagesCount':unreadMessagesCount
+    };
+  }
+}
--- lib/features/chatting/presentation/view_model/chatting_cubit/chatting_cubit.dart
+++ lib/features/chatting/presentation/view_model/chatting_cubit/chatting_cubit.dart
@@ -1,16 +1,62 @@
 
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:new_chat_with_me/core/di/locator.dart';
+import 'package:new_chat_with_me/core/shared/user_model.dart';
 
+import '../../../../../core/constants/constants.dart';
+import '../../../../../core/shared/chat_model.dart';
 import '../../../../../core/shared/shared_repo.dart';
 import 'chatting_state.dart';
 
 class ChattingCubit extends Cubit<ChattingState> {
   final SharedRepository sharedRepository;
   ChattingCubit(this.sharedRepository) : super(ChattingInitial());
+
+
   signOut()async{
     await locator<FirebaseAuth>().signOut();
+  }
 
+  listenToAllChats() async {
+
+    emit(ChattingLoading());
+    log('listenToAllChats');
+    UserModel userModel=sharedRepository.userModel;
+    try {
+
+      var chatStream = locator<FirebaseFirestore>().
+          collection(kUserCollection)
+          .doc(userModel.userId)
+          .collection(kChatsCollection)
+          .snapshots();
+
+      chatStream.listen(
+            (querySnapshot) {
+              sharedRepository.  userChats = querySnapshot.docs
+              .map((chatDoc) => ChatModel.fromJson(chatDoc.data()))
+              .toList();
+
+          if (sharedRepository.userChats.isEmpty) {
+            emit(ChattingNoChats());
+          } else {
+            emit(ChattingSuccess(sharedRepository.userChats));
+            log(sharedRepository.userChats.first.name.toString());
+          }
+        },
+        onError: (e) {
+          log('getAllChats stream error: $e');
+          emit(ChattingNoChats());
+          // emit(ChatGetUserChatsFailureState(e.toString()));
+        },
+      );
+    } catch (e) {
+      log('getAllChats initialization error: $e');
+      emit(ChattingNoChats());
+      // emit(ChatGetUserChatsFailureState(e.toString()));
+    }
   }
 }
