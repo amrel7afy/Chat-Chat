c72a7f6e46dddb321ea880034459085fc9ee228e
amrel7afy <ame.el7afy@gmail.com>
2024-05-28 19:31:08 +0300

    - add actions buttons to custom app bar.

--- lib/features/chatting/presentation/view/all_chats_view.dart
+++ lib/features/chatting/presentation/view/all_chats_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:new_chat_with_me/core/widgets/custom_app_bar.dart';
+import 'package:new_chat_with_me/features/chatting/presentation/view/all_chats_view_widgets/pop_up_app_bar_menu.dart';
 import 'all_chats_view_widgets/all_chats_floating_button.dart';
 import 'all_chats_view_widgets/all_chats_view_body.dart';
-import 'all_chats_view_widgets/pop_up_app_bar_menu.dart';
 
 class AllChatsView extends StatelessWidget {
   const AllChatsView({super.key});
@@ -10,9 +10,10 @@ class AllChatsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const Scaffold(
-       appBar:  CustomAppBar(text: 'Chat',),
+       appBar:  CustomAppBar(text: 'Chat',actions: [PopUpMenu()],),
       body: SafeArea(child: AllChatsViewBody()),
       floatingActionButton: AllChatsFloatingAction(),
     );
   }
 }
+
